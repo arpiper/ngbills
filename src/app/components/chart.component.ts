3793c52cfcd605dce93bb94c9b923759aa384828
Andrew Piper <pipera@uw.edu>
2018-02-27 16:28:22 -0800

    added the lines, and hover data. fixed the x positioning of points

--- src/app/components/chart.component.ts
+++ src/app/components/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, /*ElementRef, ViewChild*/ } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation /*ElementRef, ViewChild*/ } from '@angular/core';
 /*import { Router, ActivatedRoute } from '@angular/router';
 import { animate, keyframes, trigger, transition, style, state } from '@angular/animations';*/
 import * as d3 from 'd3';
@@ -7,17 +7,47 @@ import * as d3 from 'd3';
   moduleId: module.id,
   selector: 'chart-cmp',
   template: `
-    <div class="chart">
-      <div [id]="chartid">
-      </div>
+    <div class="chart" [id]="chartid">
     </div>
   `,
   styles: [`
-    .chart x-axis,
-    .chart y-axis {
-      stroke=width: 1px;
+    .axis path,
+    .axis line,
+    .axis text {
+      stroke-width: 1px;
+      font-size: 8pt;
+    }
+    .data-point {
+      fill: #4caf50;
+    }
+    .data-line {
+      stroke: black;
+    }
+    .hover-g {
+      fill-opacity: 0;
+      stroke-opacity: 0;
+    }
+    .hover-bar {
+      fill: var(--color-gray-dark);
+      stroke: var(--color-gray-dark);
+    }
+    .hover-value-rect {
+      fill: var(--color-gray-dark);
+    }
+    .hover-value {
+      fill: black;
+      font: 10pt mono;
+    }
+    .hover-g:hover .hover-bar,
+    .hover-g:hover .hover-value-rect {
+      stroke-opacity: 0.75;
+      fill-opacity: 0.25;
+    }
+    .hover-g:hover .hover-value {
+      fill-opacity: 1;
     }
   `],
+  encapsulation: ViewEncapsulation.None,
 })
 
 export class ChartComponent implements OnInit {
@@ -25,10 +55,10 @@ export class ChartComponent implements OnInit {
   @Input() chartid: string = 'default'
   @Input() user_settings
   settings = {
-    width: 600,
-    height: 150,
+    width: 800,
+    height: 225,
     margin: 30,
-    radius: 2,
+    radius: 3,
   }
   values: number[] = []
   x_labels: string[] = []
@@ -52,10 +82,9 @@ export class ChartComponent implements OnInit {
     if (this.data) {
       this.data.forEach((v,i) => {
         this.values.push(v.amount)
-        this.x_labels.push(v.due_date.split('/')[0])
+        this.x_labels.push(v.due_date)
       })
-      this.drawAxis()
-      this.drawPoints()
+      this.drawChart()
     }
   }
 
@@ -76,20 +105,32 @@ export class ChartComponent implements OnInit {
         .attr('transform', `translate(${this.settings.margin}, 0)`)
   }
 
+  drawChart(): void {
+    this.drawAxis()
+    this.drawLines()
+    this.drawPoints()
+    this.addHoverBar()
+  }
+
   drawPoints(): void {
     this.svg.append('g').selectAll('circle')
       .data(this.values).enter()
         .append('circle')
         .attr('class', 'data-point')
         .attr('r', this.settings.radius)
-        .attr('cx', (d, i) => {
-          console.log(this.scales.x)
-          console.log(this.scales.x(d))
-          console.log(this.scales.x.step())
-          return this.scales.x.step() / 2
-        })
+        .attr('cx', (d,i) => this.scales.x(this.x_labels[i]) + this.scales.x.step() / 2)
         .attr('cy', (d) => this.scales.y(d))
-        .attr('fill', 'black')
+  }
+
+  drawLines(): void {
+    this.svg.append('g').selectAll('line')
+      .data(this.values.slice(0, this.values.length - 1)).enter()
+        .append('line')
+        .attr('class', 'data-line')
+        .attr('x1', (d,i) => this.scales.x(this.x_labels[i]) + this.scales.x.step() / 2)
+        .attr('y1', (d) => this.scales.y(d))
+        .attr('x2', (d,i) => this.scales.x(this.x_labels[i+1]) + this.scales.x.step() / 2)
+        .attr('y2', (d,i) => this.scales.y(this.values[i+1]))
   }
 
   drawAxis(): void {
@@ -104,10 +145,39 @@ export class ChartComponent implements OnInit {
       .ticks(5)
     this.svg.append('g')
       .attr('transform', `translate(0, ${this.settings.height - this.settings.margin})`)
-      .attr('class', 'x-axis')
+      .attr('class', 'x-axis axis')
       .call(x_axis)
     this.svg.append('g')
-      .attr('class', 'y-axis')
+      .attr('class', 'y-axis axis')
       .call(y_axis)
+  }
+
+  addHoverBar(): void {
+    let g = this.svg.append('g').selectAll('g')
+      .data(this.values).enter()
+        .append('g')
+          .attr('class', 'hover-g')
+    g.append('rect')
+      .attr('class', 'hover-bar')
+      .attr('x', (d,i) => this.scales.x(this.x_labels[i]) + this.scales.x.step() / 2 - 10)
+      .attr('y', (d) => this.scales.y(d) - 10)
+      .attr('width', 20)
+      .attr('height', (d,i) => (this.settings.height - this.settings.margin) - this.scales.y(d) + 10)
+    g.append('rect')
+      .attr('class', 'hover-value-rect')
+      .attr('x', (d,i) => this.scales.x(this.x_labels[i]) + (this.scales.x.step() / 2) - 20)
+      .attr('y', (d) => this.scales.y(d) - 35)
+      .attr('width', 40)
+      .attr('height', 20)
+    g.append('text')
+      .attr('class', 'hover-value')
+      .attr('x', (d,i) => {
+        let c = this.scales.x(this.x_labels[i]) + (this.scales.x.step() / 2)
+        let w = d.toString().length * 10
+        console.log('hover text x value', c, w)
+        return c - (w / 2)
+      })
+      .attr('y', (d) => this.scales.y(d) - 20)
+      .text((d) => d)
   }
 }
